9eaac916e970c863a42cf37826d329c22387968b
Aryan Joshi <aryanjoshi014@gmail.com>
2025-02-10 14:03:14 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='First End-to-End MLOps Project.',
+    author='Aryan Joshi',
+    license='MIT',
+)
